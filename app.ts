a5dbb7f8388ebd3a1b06e9825cb6aa3a08c335dc
Radovan Kepák <radovan@kepakovi.cz>
2023-10-25 23:47:10 +0200

    wip

--- app.ts
+++ app.ts
@@ -1,14 +1,9 @@
-'use strict';
-
-const Homey = require('homey');
+import Homey from 'homey';
 
 module.exports = class AwtrixApp extends Homey.App {
 
-  /**
-   * onInit is called when the app is initialized.
-   */
   async onInit() {
     this.log('AwtrixApp has been initialized');
   }
 
-}
+};
